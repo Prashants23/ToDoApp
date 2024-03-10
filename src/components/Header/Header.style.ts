8024dbd3bfeb3818b3d91b38efb735f0491e78ca
Prashant-betterhalf <prashant@betterhalf.ai>
2024-03-10 15:18:51 +0530

    feat: created headers

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,23 @@
+import { createStyleSheet } from "react-native-unistyles";
+import { scale } from "~/utils/scale.utils";
+
+export const styleSheet = createStyleSheet(({ colors }) => {
+  return {
+    container: {
+      height: scale(48),
+      paddingHorizontal: scale(20),
+      alignItems: "center",
+      flexDirection: "row",
+      borderBottomColor: colors.gray200,
+      borderBottomWidth: 1,
+    },
+
+    title: {
+      marginLeft: scale(12),
+      fontWeight: "bold",
+      fontSize: scale(20),
+      color: colors.black,
+      width: scale(190),
+    },
+  };
+});
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,77 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { ViewStyle, TextStyle } from "react-native";
+import { useStyles } from "react-native-unistyles";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { styleSheet } from "./Header.style";
+
+/**
+ * The props for the Header component.
+ */
+interface HeaderProps {
+  /**
+   * A function that will be called when the back button is pressed.
+   */
+  onPressBack?: () => void;
+  /**
+   * The name of the screen to display in the header.
+   */
+  screenName?: string;
+  transparent?: boolean;
+  overrideStyles?: {
+    container?: ViewStyle;
+    title?: TextStyle;
+  };
+}
+
+const Header = ({
+  onPressBack,
+  screenName,
+  transparent,
+  overrideStyles,
+}: HeaderProps) => {
+  const navigation = useNavigation();
+
+  const {
+    styles,
+    theme: { colors },
+  } = useStyles(styleSheet);
+
+  // If any override backPress function is passed, use that, otherwise use the default back handler
+  const backHandler = () => {
+    if (typeof onPressBack === "function") {
+      onPressBack();
+      return;
+    }
+    navigation.goBack();
+  };
+  return (
+    <View
+      style={[
+        styles.container,
+        // eslint-disable-next-line react-native/no-inline-styles
+        {
+          backgroundColor: transparent ? colors.transparent : colors.white,
+          borderBottomWidth: transparent ? 0 : 1,
+        },
+        overrideStyles?.container,
+      ]}
+    >
+      <TouchableOpacity
+        onPress={backHandler}
+        hitSlop={{
+          top: 10,
+          bottom: 10,
+          left: 10,
+          right: 10,
+        }}
+      >
+        <Ionicons name="arrow-back" size={32} color="black" />
+      </TouchableOpacity>
+      <Text style={styles.title}>{screenName}</Text>
+    </View>
+  );
+};
+
+export default Header;
